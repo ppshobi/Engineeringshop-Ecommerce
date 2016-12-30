6759aa360782857ea911c58b9178b6f2d6055bc9
Shobi <ppshobi@gmail.com>
2016-12-31 00:19:56 +0530

    Refactoring, typo fixes

---
+++ admin/add-user.php
@@ -0,0 +1,134 @@
+<?php
+	require_once('../app/Unit.php');		
+	$message;
+		if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add-unit'])) {
+			
+			
+			$result=Unit::add($_POST['name'],$_POST['description']);
+			if ($result) {
+				$message=true;
+			}else{
+				$message=false;
+			}
+		}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Add Unit</title>
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/styles.css" rel="stylesheet">
+
+<!--Icons-->
+<script src="js/lumino.glyphs.js"></script>
+
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+	<?php include_once('nav-sidebar.php'); ?>
+	
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li class="active">Unit</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Add a Unit</h1>
+			</div>
+		</div><!--/.row-->
+				
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">					<?php 
+						if (isset($message)) {
+							if ($message) {
+								echo "<div class=\"alert bg-success\" id=\"success\" role=\"alert\">";
+								echo " <svg class=\"glyph stroked checkmark\">";
+								echo " <use xlink:href=\"#stroked-checkmark\"></use>";
+								echo " </svg>";
+								echo " Successfully Added Unit";
+								echo " <a href=\"#\" class=\"pull-right\">";
+								echo " <span class=\"glyphicon glyphicon-remove\"></span>";
+								echo " </a>";
+								echo " </div>";
+
+
+							}else{
+								echo "<div class=\"alert bg-danger\" id=\"success\" role=\"alert\">";
+								echo " <svg class=\"glyph stroked checkmark\">";
+								echo " <use xlink:href=\"#stroked-checkmark\"></use>";
+								echo " </svg>";
+								echo " Something went wrong";
+								echo " <a href=\"#\" class=\"pull-right\">";
+								echo " <span class=\"glyphicon glyphicon-remove\"></span>";
+								echo " </a>";
+								echo " </div>";
+							}
+						}
+
+					?></div>
+					<div class="panel-body">
+						<div class="col-md-6">
+							<form role="form" method="post" action="">
+							
+								<div class="form-group">
+									<label>Unit Name</label>
+									<input class="form-control" name="name" placeholder="Enter Unit Name">
+								</div>
+								<div class="form-group">
+									<label>Unit Description</label>
+									<textarea class="form-control" name="description" rows="3"></textarea>
+								</div>
+								<button type="submit" name="add-unit" class="btn btn-primary">Add Unit</button>
+								<button type="reset" class="btn btn-default">Reset</button>
+							</div>
+						</form>
+						
+					</div>
+					
+				</div>
+			</div><!-- /.col-->
+		</div><!-- /.row -->
+		
+	</div><!--/.main-->
+
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script>
+		!function ($) {
+			$(document).on("click","ul.nav li.parent > a > span.icon", function(){		  
+				$(this).find('em:first').toggleClass("glyphicon-minus");	  
+			}); 
+			$(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
+		}(window.jQuery);
+
+		$(window).on('resize', function () {
+		  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
+		})
+		$(window).on('resize', function () {
+		  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
+		})
+	</script>	
+	
+</body>
+
+</html>
--- admin/nav-sidebar.php
+++ admin/nav-sidebar.php
@@ -7,17 +7,8 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#"><span>Lumino</span>Admin</a>
-				<ul class="user-menu">
-					<li class="dropdown pull-right">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> User <span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Profile</a></li>
-							<li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> Settings</a></li>
-							<li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
-						</ul>
-					</li>
-				</ul>
+				<a class="navbar-brand" href="#"><span>Kinalath Agro Engineering</span>Admin</a>
+				
 			</div>
 							
 		</div><!-- /.container-fluid -->
@@ -33,7 +24,7 @@
 			<li><a href="index.php"><svg class="glyph stroked dashboard-dial"><use xlink:href="#stroked-dashboard-dial"></use></svg> Dashboard</a></li>
 			<li class="parent ">
 				<a href="#">
-					<span data-toggle="collapse" href="#sub-item-1"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Category
+					<span data-toggle="collapse" href="#sub-item-1"><svg class="glyph stroked folder"><use xlink:href="#stroked-folder"/></svg></span> Category
 				</a>
 				<ul class="children collapse" id="sub-item-1">
 					<li>
@@ -43,7 +34,7 @@
 					</li>
 					<li>
 						<a class="" href="view-categories.php">
-							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> View Category
+							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Manage Category
 						</a>
 					</li>
 										
@@ -51,7 +42,7 @@
 			</li>
 			<li class="parent ">
 				<a href="#">
-					<span data-toggle="collapse" href="#product"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Product
+					<span data-toggle="collapse" href="#product"><svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg></span> Product
 				</a>
 				<ul class="children collapse" id="product">
 					<li>
@@ -61,7 +52,7 @@
 					</li>
 					<li>
 						<a class="" href="view-products.php">
-							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> View Products
+							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Manage Products
 						</a>
 					</li>
 								
@@ -69,7 +60,20 @@
 			</li>
 			<li class="parent ">
 				<a href="#">
-					<span data-toggle="collapse" href="#unit"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Unit
+					<span data-toggle="collapse" href="#order"><svg class="glyph stroked table"><use xlink:href="#stroked-table"/></svg></span> Orders
+				</a>
+				<ul class="children collapse" id="order">					
+					<li>
+						<a class="" href="view-products.php">
+							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Manage Orders
+						</a>
+					</li>
+								
+				</ul>
+			</li>
+			<li class="parent ">
+				<a href="#">
+					<span data-toggle="collapse" href="#unit"><svg class="glyph stroked hourglass"><use xlink:href="#stroked-hourglass"/></svg></span> Unit
 				</a>
 				<ul class="children collapse" id="unit">
 					<li>
@@ -79,42 +83,33 @@
 					</li>
 					<li>
 						<a class="" href="view-units.php">
-							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> View Units
+							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Manage Units
 						</a>
 					</li>
 								
 				</ul>
 			</li>
-			<li><a href="widgets.php"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg> Widgets</a></li>
-			<li class="active"><a href="charts.php"><svg class="glyph stroked line-graph"><use xlink:href="#stroked-line-graph"></use></svg> Charts</a></li>
-			<li><a href="tables.php"><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> Tables</a></li>
-			<li><a href="forms.php"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg> Forms</a></li>
-			<li><a href="panels.php"><svg class="glyph stroked app-window"><use xlink:href="#stroked-app-window"></use></svg> Alerts &amp; Panels</a></li>
-			<li><a href="icons.php"><svg class="glyph stroked star"><use xlink:href="#stroked-star"></use></svg> Icons</a></li>
 			<li class="parent ">
 				<a href="#">
-					<span data-toggle="collapse" href="#sub-item-1"><svg class="glyph stroked chevron-down"><use xlink:href="#stroked-chevron-down"></use></svg></span> Dropdown 
+					<span data-toggle="collapse" href="#user"><svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg></span> Users
 				</a>
-				<ul class="children collapse" id="sub-item-1">
+				<ul class="children collapse" id="user">
 					<li>
-						<a class="" href="#">
-							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Sub Item 1
+						<a class="" href="add-user.php">
+							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Add User
 						</a>
 					</li>
 					<li>
-						<a class="" href="#">
-							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Sub Item 2
+						<a class="" href="view-users.php">
+							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Manage Users
 						</a>
 					</li>
-					<li>
-						<a class="" href="#">
-							<svg class="glyph stroked chevron-right"><use xlink:href="#stroked-chevron-right"></use></svg> Sub Item 3
-						</a>
-					</li>
+								
 				</ul>
 			</li>
+			
 			<li role="presentation" class="divider"></li>
-			<li><a href="login.php"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Login Page</a></li>
+			<li><a href="login.php?purpose=logout"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Logout</a></li>
 		</ul>
 		
 	</div><!--/.sidebar-->
