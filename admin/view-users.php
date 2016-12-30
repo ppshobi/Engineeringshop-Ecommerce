eefdf3f7b2165c4a23dbcf8669321eb6fec98aaf
Shobi <ppshobi@gmail.com>
2016-12-31 00:33:08 +0530

    delete user done

--- admin/view-users.php
+++ admin/view-users.php
@@ -1,7 +1,7 @@
 <?php
-require_once('../app/Unit.php');		
+require_once('../app/User.php');		
 	$message;
-	$units=Unit::getAll();
+	$users=User::getAll();
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,13 +28,8 @@ require_once('../app/Unit.php');
 <body>
 	<?php include_once('nav-sidebar.php'); ?>
 		
-	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
-		<div class="row">
-			<ol class="breadcrumb">
-				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-				<li class="active">Icons</li>
-			</ol>
-		</div><!--/.row-->
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">		
+		
 		
 		<div class="row">
 			<div class="col-lg-12">
@@ -47,25 +42,35 @@ require_once('../app/Unit.php');
 			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						Edit and delete units from here
+						Edit and delete users
 					</div>
 
 					<div class="panel-body">
 						<table data-toggle="table" data-url="tables/data1.json"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
 						    <thead>
 						    <tr>
-						        <th data-field="state" data-checkbox="true" >Unit Name</th>
-						        <th data-field="id" data-sortable="true">Unit Description</th>
+						        <th data-field="state" data-checkbox="true" >User Name</th>
+						        <th data-field="id" data-sortable="true">User Email</th>
+						        <th data-field="id" data-sortable="true">Phone Number</th>
+						        <th data-field="id" data-sortable="true">User Level</th>
 						        <th data-field="name"  data-sortable="true">Edit</th>
 						        <th data-field="price" data-sortable="true">Delete</th>
 						    </tr>
 						    <?php
-						    foreach ($units as $unit) {
+						    foreach ($users as $user) {
+						    	$userlevel=$user['level'];
+						    	if($userlevel==1){
+						    		$userlevel="Admin";
+						    	}else{
+						    		$userlevel="Customer";
+						    	}
 						    	echo "<tr>";
-						    		echo "<td>".$unit['name']."</td>";
-						    		echo "<td>".$unit['descr']."</td>";
-						    		echo "<td>"."<a href=\"edit-unit.php?id={$unit['id']}\">Edit" ."</a> </td>";
-						    		echo "<td>"."<a href=\"delete-unit.php?id={$unit['id']}\">Delete"."</a> </td>";
+						    		echo "<td>".$user['name']."</td>";
+						    		echo "<td>".$user['username']."</td>";
+						    		echo "<td>".$user['phone']."</td>";
+						    		echo "<td>".$userlevel."</td>";
+						    		echo "<td>"."<a href=\"edit-user.php?id={$user['id']}\">Edit" ."</a> </td>";
+						    		echo "<td>"."<a href=\"delete-user.php?id={$user['id']}\">Delete"."</a> </td>";
 						   		echo "</tr>";
 						    }
 						    ?>
