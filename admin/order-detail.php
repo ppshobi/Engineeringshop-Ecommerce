75e502a4c6c0475b55cb9ed9fea638798f1339de
Shobi <ppshobi@gmail.com>
2017-01-01 23:43:21 +0530

    Bread crumb removed

--- admin/order-detail.php
+++ admin/order-detail.php
@@ -47,12 +47,7 @@
 	<?php include_once('nav-sidebar.php'); ?>
 		
 	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
-		<div class="row">
-			<ol class="breadcrumb">
-				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-				<li class="active">Icons</li>
-			</ol>
-		</div><!--/.row-->
+		<!--/.row-->
 		
 		<div class="row">
 			<div class="col-lg-12">
