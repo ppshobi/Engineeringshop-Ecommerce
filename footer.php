3a85745a89f59e4b92ae2848be36fbcda5673ee6
Shobi <ppshobi@gmail.com>
2017-01-01 20:20:32 +0530

    Footer typos

--- footer.php
+++ footer.php
@@ -49,78 +49,26 @@
                         </ul>
                     </div>
                     <div class="col-md-3 col-sm-6 tags-footer">
-                        <div class="footer-top-title"><h4 class="title-footer">Hottags</h4></div>
+                        <div class="footer-top-title"><h4 class="title-footer">Hot Tags</h4></div>
                         <ul class="tags footer-tags">
-                            <li class="tags-item"><a href="#">Kingston</a></li>
-                            <li class="tags-item"><a href="#">Android</a></li>
-                            <li class="tags-item"><a href="#">Mini book</a></li>
-                            <li class="tags-item"><a href="#">Mini USB</a></li>
-                            <li class="tags-item"><a href="#">TMD</a></li>
-                            <li class="tags-item"><a href="#">IPhone</a></li>
-                            <li class="tags-item"><a href="#">PC Black</a></li>
-                            <li class="tags-item"><a href="#">Sport</a></li>
-                            <li class="tags-item"><a href="#">Flat</a></li>
-                            <li class="tags-item"><a href="#">Shirts</a></li>
-                            <li class="tags-item"><a href="#">Arrow Shirts</a></li>
+                            <li class="tags-item"><a href="#">Pumpset</a></li>
+                            <li class="tags-item"><a href="#">Water Solutions</a></li>
+                            <li class="tags-item"><a href="#">Motors</a></li>
+                            <li class="tags-item"><a href="#">Monoblock</a></li>
+                            <li class="tags-item"><a href="#">Submersible</a></li>
+                            <li class="tags-item"><a href="#">Springlers</a></li>
+                            <li class="tags-item"><a href="#">Pipes</a></li>
+                            <li class="tags-item"><a href="#">Engineering</a></li>
+                            <li class="tags-item"><a href="#">Consultancy</a></li>
+                            <li class="tags-item"><a href="#">Supervising</a></li>
+                            <li class="tags-item"><a href="#">Fittings</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="footer-mid">
-                <div class="footer-mid-text">
-                    <figure><img src="assets/images/check.jpg" alt="check" height="30" width="30"></figure>
-                    <p>
-                        Help improve your experience on SuperMarket,<span> join our Customer Experience Improvement Program now.
-                                    </span> Plus,<span>
-                                        tell us what you think of this page</span> !
-                    </p>
-                </div>
-                <!--<figure><img src="assets/images/img-logo-footer.jpg" alt="img-footer" height="60" width="1170"></figure>-->
-                <ul>
-                    <li><a href="#"><img src="assets/images/d1.jpg" alt="d" width="46" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d2.jpg" alt="d" width="51" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d3.jpg" alt="d" width="60" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d4.jpg" alt="d" width="44" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d5.jpg" alt="d" width="42" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d6.jpg" alt="d" width="85" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d7.jpg" alt="d" width="89" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d8.jpg" alt="d" width="92" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d9.jpg" alt="d" width="65" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d10.jpg" alt="d" width="91" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d11.jpg" alt="d" width="96" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d12.jpg" alt="d" width="64" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d13.jpg" alt="d" width="131" height="41"></a></li>
-                    <li><a href="#"><img src="assets/images/d14.jpg" alt="d" width="94" height="41"></a></li>
-                </ul>
-            </div>
+            
             <div class="footer-bottom">
-                <ul class="menu-footer-bottom">
-                    <li class="menu-footer-bottom-item"><a href="#">Promotions</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">My Orders</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Help</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Site Map</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Customer Service</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Most Populars</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Best Sellers</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">New Arrivals</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Special Products</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Manufacturers</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Our Stores</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Shipping</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Blog</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Payments</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Payments</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Refunds Terms & Conditions</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Policy</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Policy</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Shipping</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Payments</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Returns</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Refunds</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Warrantee</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">FAQ</a></li>
-                    <li class="menu-footer-bottom-item"><a href="#">Contact</a></li>
-                </ul>
+                
                 <div class="copyright">
                     <i class="fa fa-copyright" aria-hidden="true"></i>
                     Copyright 2017 <a href="#">Kinalath Agro Engineering</a> All rights reserved.
