c963b33b334941e797febb909ff961225a0b5839
Shobi <ppshobi@gmail.com>
2017-05-03 15:49:03 +0530

    Dashboard icon change

--- admin/index.php
+++ admin/index.php
@@ -48,6 +48,21 @@ require_once('../app/Unit.php');
 		
 		<div class="row">
 			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="panel panel-red panel-widget">
+					<div class="row no-padding">
+						<div class="col-sm-3 col-lg-5 widget-left">
+							<svg class="glyph stroked clipboard with paper">
+								<use xlink:href="#stroked-clipboard-with-paper"/>
+							</svg>
+						</div>
+						<div class="col-sm-9 col-lg-7 widget-right">
+							<div class="large"><?php echo "&#8377; ".Order::total_sales_sum(); ?></div>
+							<div class="text-muted">Total Sales Amount</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
 				<div class="panel panel-blue panel-widget ">
 					<div class="row no-padding">
 						<div class="col-sm-3 col-lg-5 widget-left">
@@ -86,22 +101,23 @@ require_once('../app/Unit.php');
 					</div>
 				</div>
 			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-red panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?php echo Product::getCount(); ?></div>
-							<div class="text-muted">Products</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			
 		</div><!--/.row-->
 		
 		<div class="row">
+			<div class="col-xs-12 col-md-6 col-lg-3">
+					<div class="panel panel-red panel-widget">
+						<div class="row no-padding">
+							<div class="col-sm-3 col-lg-5 widget-left">
+								<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
+							</div>
+							<div class="col-sm-9 col-lg-7 widget-right">
+								<div class="large"><?php echo Product::getCount(); ?></div>
+								<div class="text-muted">Products</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			<div class="col-xs-12 col-md-6 col-lg-3">
 				<div class="panel panel-blue panel-widget ">
 					<div class="row no-padding">
@@ -119,7 +135,7 @@ require_once('../app/Unit.php');
 				<div class="panel panel-blue panel-widget ">
 					<div class="row no-padding">
 						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
+							<svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
 						</div>
 						<div class="col-sm-9 col-lg-7 widget-right">
 							<div class="large"><?php echo Question::count() ?></div>
@@ -132,7 +148,7 @@ require_once('../app/Unit.php');
 				<div class="panel panel-teal panel-widget">
 					<div class="row no-padding">
 						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
+							<svg class="glyph stroked hourglass"><use xlink:href="#stroked-hourglass"/></svg>
 						</div>
 						<div class="col-sm-9 col-lg-7 widget-right">
 							<div class="large"><?php echo Unit::count(); ?></div>
@@ -141,19 +157,7 @@ require_once('../app/Unit.php');
 					</div>
 				</div>
 			</div>
-			<div class="col-xs-12 col-md-6 col-lg-3">
-				<div class="panel panel-red panel-widget">
-					<div class="row no-padding">
-						<div class="col-sm-3 col-lg-5 widget-left">
-							<svg class="glyph stroked app-window-with-content"><use xlink:href="#stroked-app-window-with-content"></use></svg>
-						</div>
-						<div class="col-sm-9 col-lg-7 widget-right">
-							<div class="large"><?php echo "&#8377; ".Order::total_sales_sum(); ?></div>
-							<div class="text-muted">Total Sales Amount</div>
-						</div>
-					</div>
-				</div>
-			</div>
+
 		</div><!--/.row-->
 		
 	</div>	<!--/.main-->
